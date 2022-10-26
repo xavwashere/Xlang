b20f9ffc3aa63ac32db5eb7c60dc52dc41573aa8
Exyrus <97399129+Exyrus@users.noreply.github.com>
2022-10-26 19:58:36 +0100

    big commit - lexer!!

--- src/include/lexer.h
+++ src/include/lexer.h
@@ -1,5 +1,6 @@
 #ifndef LEXER_H
 #define LEXER_H
+#include "tokens.h"
 
 typedef struct LEXER_STRUCT{
     char c;
@@ -11,6 +12,13 @@ typedef struct LEXER_STRUCT{
 lexer_T* init_lexer(char* contents);
 
 void lexer_advance(lexer_T* lexer);
-
 void lexer_skip_whitespace(lexer_T* lexer);
+token_T* lexer_get_next_token(lexer_T* lexer);
+token_T* lexer_collect_string(lexer_T* lexer);
+token_T* lexer_collect_id(lexer_T* lexer);
+token_T* lexer_advance_with_token(lexer_T* lexer, token_T* token);
+
+char* lexer_get_current_char_as_string(lexer_T* lexer);
+
+
 #endif
--- src/main.c
+++ src/main.c
@@ -1,7 +1,17 @@
 #include <stdio.h>
+#include "include/lexer.h"
 
 int main(int argc, char* argv[]){
-    printf("test");
+    lexer_T* lexer = init_lexer(
+       "def var = \"var\";"
+       "print(var)"
+    );
+
+    token_T* token = (void*)0;
+
+    while ((token = lexer_get_next_token(lexer)) -> type != (void*)0){
+        printf("TOKEN(%d, %s)\n", token -> type, token -> value);
+    }
 
     return 0;
 }
